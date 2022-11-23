30227117ca61a7eeec909d0d7fc1219cb759aea1
nitayke <nitayke@gmail.com>
2022-11-24 01:15:34 +0200

    ok

--- toranim.py
+++ toranim.py
@@ -6,12 +6,10 @@ from datetime import datetime
 
 FOLDER = 'C:/Users/user/Desktop/toranim/'
 VERSIONS_FOLDER = 'versions'
+XL_NAME = 'תורנים.xlsx'
 TABLE_SIZE = 9 # x9
-# shishi, regular
-TABLE_CELLS = '''13, 14, 22, 23
-40, 41, 49, 50
-
-9, 10, 18, 19
+# regular, shishi
+TABLE_CELLS = '''9, 10, 18, 19
 11, 12, 20, 21
 15, 16, 17, 24, 25
 36, 37, 45, 46
@@ -19,19 +17,25 @@ TABLE_CELLS = '''13, 14, 22, 23
 42, 43, 44, 51, 52
 63, 64, 72, 73
 65, 66, 74, 75
-67, 68, 76, 77'''.split('\n')
+67, 68, 76, 77
+
+13, 14, 22, 23
+40, 41, 49, 50'''.split('\n')
+
+REGULAR = MITUTA = 0
+SHISHI = HAVRUTA = 1
 
 
 class Excel:
-    def __init__(self, filename):
-        self.wb = openpyxl.load_workbook(FOLDER + filename)
+    def __init__(self):
+        self.wb = openpyxl.load_workbook(FOLDER + XL_NAME)
         self.ws = self.wb.active
         self.toranim_data = {} # {name: [all, shishi]}
         self.havruta_data = []
     
     def extract(self):
         for i in self.ws['A2:D' + str(self.ws.max_row)]:
-            self.toranim_data[i[0].value] = [i[2].value, i[3].value]
+            self.toranim_data[i[0].value] = [i[2].value, i[3].value] # regular and shishi
         
         for i in self.ws['F2:G' + str(self.ws.max_row // 2)]:
             if not i[0].value:
@@ -50,9 +54,9 @@ class Excel:
     def update(self, toranim_data):
         self.wb.save(f'{FOLDER}{VERSIONS_FOLDER}/{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}.xlsx')
         for i, name in enumerate(toranim_data):
-            self.ws.cell(i+2, 3).value = toranim_data[name][0]
-            self.ws.cell(i+2, 4).value = toranim_data[name][1]
-        self.wb.save(FOLDER+'try.xlsx')
+            self.ws.cell(i+2, 3).value = toranim_data[name][REGULAR]
+            self.ws.cell(i+2, 4).value = toranim_data[name][SHISHI]
+        self.wb.save(FOLDER + XL_NAME)
 
 
 class Tkinter:
@@ -64,7 +68,7 @@ class Tkinter:
         frame.pack()
         c = Calculate()
         ttk.Button(frame, text='סבב רגיל', command=lambda: c.regular_sevev()).pack()
-        ttk.Button(frame, text='סבב מיוחד', command=lambda: c.special_sevev()).pack()
+        # ttk.Button(frame, text='סבב מיוחד', command=lambda: c.special_sevev()).pack()
         cls.root.mainloop()
 
     @classmethod
@@ -101,97 +105,80 @@ class Tkinter:
 
 class Calculate:
     def __init__(self):
-        self.excel = Excel('תורנים.xlsx')
+        self.excel = Excel()
         self.havrutot, self.toranim = self.excel.extract()
-        self.regular_result, self.shishi_result = [[], []], [[], []]
-        self.regular_count, self.shishi_count = 0, 0
+        self.results = [[[], []], [[], []]] # [regular: [lonely, havruta], shishi: [lonely, havruta]]
+        self.count = [0, 0] # regular and shishi
 
     def get_united_min_list(self):
         res = []
-        min_shishi = min([i[1] for i in self.toranim.values()])
-        min_toranut = min([i[0] for i in self.toranim.values()])
+        min_shishi = min([i[SHISHI] for i in self.toranim.values()])
+        min_toranut = min([i[REGULAR] for i in self.toranim.values()])
         for i in self.toranim:
-            if self.toranim[i][0] == min_toranut and self.toranim[i][1] == min_shishi:
+            if self.toranim[i][REGULAR] == min_toranut and self.toranim[i][SHISHI] == min_shishi:
                 res.append(i)
         return res
 
     def get_regular_min_list(self):
         res = []
-        min_toranut = min([i[0] for i in self.toranim.values()]) # TODO: improve double
+        min_toranut = min([i[REGULAR] for i in self.toranim.values()]) # TODO: improve double
         for i in self.toranim:
-            if self.toranim[i][0] == min_toranut:
+            if self.toranim[i][REGULAR] == min_toranut:
                 res.append(i)
         return res
 
-    def add_regular(self, name, has_havruta):
-        self.regular_result[has_havruta].append(name)
-        self.regular_count -= 1
-        self.toranim[name][0] += 1
-
-    def add_shishi(self, name, has_havruta):
-        self.shishi_result[has_havruta].append(name)
-        self.shishi_count -= 1
-        self.toranim[name][0] += 1
-        self.toranim[name][1] += 1
+    def add(self, name, has_havruta, shishi):
+        self.results[shishi][has_havruta].append(name)
+        self.count[shishi] -= 1
+        self.toranim[name][REGULAR] += 1
+        if shishi:
+            self.toranim[name][SHISHI] += 1
     
     def add_last_toran(self, shishi):
         if shishi:
             list = self.get_united_min_list()
-            func = self.add_shishi
         else:
             list = self.get_regular_min_list()
-            func = self.add_regular
-        for i in list.copy():
+        for i in list.copy(): # TODO: remove copy
             if self.excel.get_havruta(i) == '':
-                func(i, False)
+                self.add(i, False, shishi)
                 return True
         return False
 
+    def util(self, shishi):
+        if len(self.min_lists[shishi]) <= self.count[shishi]:
+            self.results[shishi][MITUTA] = self.min_lists[shishi] # TODO: MAYBE NOT SUPPOSED TO BE LONELY
+            self.count[shishi] -= len(self.min_lists[shishi])  # need to add more to fill the required
+            for i in self.min_lists[shishi]:
+                self.toranim[i][shishi] += 1
+
+        for i in self.min_lists[shishi]:
+            if self.count[shishi] == 1 and not self.add_last_toran(shishi):
+                self.add(i, False, shishi)
+            if self.count[shishi] == 0:
+                break
+            current_havruta = self.excel.get_havruta(i)
+            if current_havruta in self.min_lists[shishi]:
+                self.add(current_havruta, True, shishi)
+                self.add(i, True, shishi)
+            else:
+                self.add(i, False, shishi)
+
     def calculate(self, regular_count, shishi_count):
-        self.regular_count, self.shishi_count = regular_count, shishi_count
-        self.united_min_list = self.get_united_min_list()  # toranut and shishi has minimum values
-        
-        if len(self.united_min_list) <= self.shishi_count: # TODO
-            self.shishi_result[0] = self.united_min_list
-            self.shishi_count -= len(self.united_min_list)  # need to add more to fill the required
-
-        for i in self.united_min_list:
-            if self.shishi_count == 1 and not self.add_last_toran(True):
-                self.add_shishi(i, False)
-            if self.shishi_count == 0:
-                break
-            current_havruta = self.excel.get_havruta(i)
-            if current_havruta in self.united_min_list:
-                self.add_shishi(current_havruta, True)
-                self.add_shishi(i, True)
-            else:
-                self.add_shishi(i, False)
-
-        self.regular_min_list = self.get_regular_min_list()
-
-        if len(self.regular_min_list) <= self.regular_count:
-            self.regular_result[0] = self.regular_min_list
-            self.regular_count -= len(self.regular_min_list)
-
-        for i in self.regular_min_list:
-            if self.regular_count == 1 and not self.add_last_toran(False):
-                self.add_regular(i, False)
-            if not self.regular_count:
-                break
-            current_havruta = self.excel.get_havruta(i)
-            if current_havruta in self.regular_min_list:
-                self.add_regular(current_havruta, True)
-                self.add_regular(i, True)
-            else:
-                self.add_regular(i, False)
-
+        self.count = [shishi_count, regular_count]
+        self.min_lists = [[], self.get_united_min_list()] # regular and united
+        # united is where the regular and shishi have minimum values
+        self.util(SHISHI)
+        self.min_lists[0] = self.get_regular_min_list()
+        self.util(REGULAR)
         self.put_results()
         self.excel.update(self.toranim)
         Tkinter.close()
 
     def put_results(self):
-        word = Word(self.shishi_count, self.regular_count)
-        word.fill_table(self.shishi_result, self.regular_result)
+        word = Word(self.count)
+        word.fill_table(self.results)
+        word.update()
 
     def regular_sevev(self):
         self.calculate(38, 8)
@@ -202,12 +189,12 @@ class Calculate:
 
 
 class Word:
-    def __init__(self, shishi_count, regular_count):
+    def __init__(self, count):
         self.template_doc = Document(FOLDER + 'template.docx')
         self.table = self.template_doc.tables[0]
-        self.result = Document(FOLDER+'תוצאה.docx')
-        self.shishi_count, self.regular_count = shishi_count, regular_count
-        self.table_cells = [[], []] # shishi, regular
+        self.result = Document(FOLDER + 'תוצאה.docx')
+        self.regular_count, self.shishi_count = count
+        self.table_cells = [[], []] # regular, shishi
         index = 0
         for i in TABLE_CELLS:
             if i == '':
@@ -215,25 +202,28 @@ class Word:
                 continue
             self.table_cells[index].append([int(i) for i in i.split(', ')])
 
-    def fill_table(self, shishi_result, regular_result):
-        results = [shishi_result, regular_result]
-        counts = [[0, 0], [0, 0]] # shishi [lonely, havruta] regular [lonely, havruta]
-        lengths = ((len(shishi_result[0]), len(shishi_result[1])),
-            (len(regular_result[0]), len(regular_result[1])))
-        for i in range(len(self.table_cells)): # 2 iterations - shishi and regular
-            for j in self.table_cells[i]: # over groups
+    def fill_table(self, results):
+        counts = [[0, 0], [0, 0]] # regular [lonely, havruta], shishi [lonely, havruta]
+
+        lengths = ((len(results[REGULAR][MITUTA]), len(results[REGULAR][HAVRUTA])),
+            ((len(results[SHISHI][MITUTA]), len(results[SHISHI][HAVRUTA]))))
+        
+        for i in range(len(self.table_cells)): # 2 iterations - regular and shishi
+            for j in self.table_cells[1-i]: # over groups. I want to put shishi first
                 flag = False
                 counter = 0
                 for k in j:
-                    if counts[i][1] < lengths[i][1] and counter < len(j) - 1 or flag:
-                        self.table.rows[k // TABLE_SIZE].cells[k % TABLE_SIZE].text = results[i][1][counts[i][1]]
+                    if counts[i][HAVRUTA] < lengths[i][HAVRUTA] and counter < len(j) - 1 or flag:
+                        self.table.rows[k // TABLE_SIZE].cells[k % TABLE_SIZE].text = results[i][HAVRUTA][counts[i][HAVRUTA]]
                         flag = not flag
-                        counts[i][1] += 1
+                        counts[i][HAVRUTA] += 1
                     else:
-                        self.table.rows[k // TABLE_SIZE].cells[k % TABLE_SIZE].text = results[i][0][counts[i][0]]
-                        counts[i][0] += 1
+                        self.table.rows[k // TABLE_SIZE].cells[k % TABLE_SIZE].text = results[i][MITUTA][counts[i][MITUTA]]
+                        counts[i][MITUTA] += 1
                     counter += 1
-        
+
+    def update(self):
+        self.result.add_paragraph(datetime.now().strftime('%m/%d/%Y, %H:%M:%S'))
         self.result.element.body.append(self.template_doc.element.body[0])
         self.result.save(FOLDER+'תוצאה.docx')
 
