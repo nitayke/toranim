405a7e7dd25a8194186b07097536266a0e74b492
Shalom Prinz <shalom.prinz@gmail.com>
2024-02-06 21:03:40 +0200

    run clean_compile in order to compile into exe

---
+++ מטבח/clean.bat
@@ -0,0 +1,4 @@
+rmdir /s build
+del toranim.spec
+move dist\toranim.exe .
+rmdir dist
--- מטבח/compile.bat
+++ מטבח/compile.bat
@@ -1,5 +1,1 @@
 pyinstaller --onefile --noconsole toranim.py
-rmdir /s build
-del toranim.spec
-move dist\toranim.exe .
-rmdir dist
